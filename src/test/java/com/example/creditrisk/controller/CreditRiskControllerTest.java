bf0086b5a006a7502215424ef4bae005844cd1e5
rajithr <r.rajith@gmail.com>
2026-07-17 21:21:03 +0530

    Fix import statement for MockBean in tests

--- src/test/java/com/example/creditrisk/controller/CreditRiskControllerTest.java
+++ src/test/java/com/example/creditrisk/controller/CreditRiskControllerTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.MockBean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
